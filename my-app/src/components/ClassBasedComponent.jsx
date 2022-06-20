e8af4157c9bdfe33894a96cfce9f04c76726083c
ArdeeWest <teleliterator@gmail.com>
2022-06-20 17:39:06 +0300

    Create ClassBasedComponent.jsx

---
+++ my-app/src/components/ClassBasedComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class ClassBasedComponent extends Component {
+  render() {
+    return (
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+        ipsam, doloribus sunt cumque facere ipsa animi ab illo quisquam
+        distinctio?
+      </p>
+    );
+  }
+}
+
+export default ClassBasedComponent;
